23b9d56bc45642b7b7d9256aa3344b80d7e688d6
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-10-30 10:03:41 -0700

    updated code

--- assets/app.js
+++ assets/app.js
@@ -11,8 +11,15 @@ $(document).ready(function(){
             url: "http://api.openweathermap.org/data/2.5/weather?q=" +searchTearm +"&APPID=66fa9b97478bf2b9d156815cabfc54c0",
             dataType: "json",
             success: function(data){
+                // was able to console log lat and long//
                 console.log(data);
             }
         })
     }
 })
+// .then(function (response) {
+
+//     $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+//     $(".wind").text("Wind Speed: " + response.wind.speed);
+//     $(".humidity").text("Humidity: " + response.main.humidity);
+//     $(".temp").text("Temperature (F) " + response.main.temp);
