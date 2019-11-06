abcaf134ce80c0d5a8493cde7a488f56f0f5a1ff
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-11-06 14:32:00 -0700

    updated https

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     function weatherAPI(searchTearm){
         $.ajax({
             type:"GET",
-            url: "http://api.openweathermap.org/data/2.5/weather?q=" +searchTearm +"&APPID=66fa9b97478bf2b9d156815cabfc54c0",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" +searchTearm +"&APPID=66fa9b97478bf2b9d156815cabfc54c0",
             dataType: "json",
             success: function(data){
                 // was able to console log lat and long//
